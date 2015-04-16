65afc6f9c392da396998c7d1c8f2b122d07042ee
Tobias Kahn <tobiasbkahn@gmail.com>
2015-04-16 10:50:47 -0400

    Show page styling, add validations to models

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -2,6 +2,12 @@ class Stop < ActiveRecord::Base
   has_many :tour_stops
   has_many :tours, through: :tour_stops
 
+  validates :user, presence: true
+  validates :latitude, presence: true, numericality: { only_integer: true }
+  validates :longitude, presence: true, numericality: { only_integer: true }
+  validates :stop_length, presence: true, numericality: { only_integer: true }
+
+
   def new_stop_number
     if tour_stops.last == nil
       new_stop = 1
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,6 +1,9 @@
 class Tour < ActiveRecord::Base
   has_many :tour_stops, dependent: :destroy
   has_many :stops, through: :tour_stops, dependent: :destroy
+  
+  validates :user, presence: true
+  validates :name, presence: true
 
   def owner?(current_user)
     self.user_id == current_user.id unless current_user == nil
