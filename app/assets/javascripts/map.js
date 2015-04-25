6849a42f992d3b889fa252977ec07780f0d452fc
Tobias Kahn <tobiasbkahn@gmail.com>
2015-04-25 16:15:25 -0400

    Finish cleaning up JS with new api, all methods except calculate_toral_route() restored

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,32 +1,30 @@
 var current_url = location.href
 var current_url = current_url + '.json'
+$.get(current_url, function(json_tour_data) {
 
-// Need to add conditions for failure
-function get_location(location) {
-  navigator.geolocation.getCurrentPosition(show_map);
-};
+  // Need to add conditions for failure
+  function get_location(location) {
+    navigator.geolocation.getCurrentPosition(show_map);
+  };
 
-function show_map(loc) {
-  window.user_latitude = loc.coords.latitude;
-  window.user_longitude = loc.coords.longitude
+  function show_map(loc) {
+    window.user_latitude = loc.coords.latitude;
+    window.user_longitude = loc.coords.longitude
 
-  $.ajax({
-    method: 'PUT',
-    url: '/',
-    data: { user_latitude: user_latitude,  user_longitude: user_longitude },
-    dataType: 'json'
-  });
-};
+    // $.ajax({
+    //   method: 'PUT',
+    //   url: '/',
+    //   data: { user_latitude: user_latitude,  user_longitude: user_longitude },
+    //   dataType: 'json'
+    // });
+  };
 
-get_location(location);
+  get_location(location);
 
-var directionsDisplay;
-var directionsService = new google.maps.DirectionsService();
+  var directionsDisplay;
+  var directionsService = new google.maps.DirectionsService();
 
-function initialize() {
-
-  $.get('1.json', function(json_tour_data) {
-
+  function initialize() {
     directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true});
 
     var center = { lat: json_tour_data["tour"].tour_stops[0].stop.latitude, lng: json_tour_data["tour"].tour_stops[0].stop.longitude + .015 };
@@ -64,6 +62,7 @@ function initialize() {
         map.setCenter(marker.getPosition());
       });
     });
+  };
 
   function calc_route_to_start() {
     var start = new google.maps.LatLng(user_latitude, user_longitude);
@@ -82,84 +81,84 @@ function initialize() {
     });
   };
 
-    function calc_total_route() {
+  function calc_total_route() {
+    var x = json_tour_data["tour"]["tour_stops"].length - 1
+    var start = new google.maps.LatLng(json_tour_data["tour"]["tour_stops"][0]["stop"].latitude, json_tour_data["tour"]["tour_stops"][0]["stop"].longitude);
+    var end = new google.maps.LatLng(json_tour_data["tour"]["tour_stops"][x]["stop"].latitude, json_tour_data["tour"]["tour_stops"][x]["stop"].longitude);
+    var waypts = [];
+
+    // loop over all destinations except start and end. For each push the destination into a new point
+    json_tour_data["tour"]["tour_stops"].forEach(function(stop) {
+      waypts.push({
+              location:new google.maps.LatLng(stop["stop"].latitude, stop["stop"].longitude),
+              stopover:true});
+    });
+
+    var request = {
+        origin: start,
+        waypoints: waypts,
+        destination: end,
+        optimizeWaypoints: true,
+        travelMode: google.maps.TravelMode.WALKING
+    };
+
+    directionsService.route(request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        leg_durations = []
+        leg_lengths = []
+
+        // I need to loop through the response and create an array of durations
+        response.routes[0].legs.forEach(function(duration) {
+            leg_durations.push(duration.duration);
+        });
+
+        response.routes[0].legs.forEach(function(length) {
+            leg_lengths.push(length.distance);
+        });
+
+        // $.ajax({
+        //   method: 'PUT',
+        //   url: x,
+        //   data: { tour_legs: leg_durations, leg_lengths: leg_lengths },
+        //   dataType: 'json'
+        // });
+      }
+    });
+  };
+
+  calc_total_route();
+
+  function calc_route(stop_div_id, start_lat, start_long, end_lat, end_long) {
+   var start = new google.maps.LatLng(start_lat, start_long);
+   var end = new google.maps.LatLng(end_lat, end_long);
+
+   var request = {
+       origin: start,
+       destination: end,
+       travelMode: google.maps.TravelMode.WALKING
+   };
+
+   directionsService.route(request, function(response, status) {
+     if (status == google.maps.DirectionsStatus.OK) {
+       directionsDisplay.setDirections(response);
+     }
+   });
+
+   directionsDisplay.setPanel(document.getElementById(stop_div_id + '_directions_text'));
+  };
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+  $("#" + json_tour_data.tour.tour_stops[0].stop.div_id + '_directions').click(function() {
+    calc_route_to_start();
+  });
+
+
+  // This loop both declares which divs should trigger calcRoute, and passes in the parameters that calcRoute will use
+  json_tour_data["tour"]["tour_stops"].forEach(function(stop) {
+    $("#" + stop.stop.div_id + "_directions").click(function() {
       var x = json_tour_data["tour"]["tour_stops"].length - 1
-      var start = new google.maps.LatLng(json_tour_data["tour"]["tour_stops"][0]["stop"].latitude, json_tour_data["tour"]["tour_stops"][0]["stop"].longitude);
-      var end = new google.maps.LatLng(  json_tour_data["tour"]["tour_stops"][x]["stop"].latitude,   json_tour_data["tour"]["tour_stops"][x]["stop"].longitude);
-      var waypts = [];
-
-      // loop over all destinations except start and end. For each push the destination into a new point
-      json_tour_data["tour"]["tour_stops"].forEach(function(stop) {
-        waypts.push({
-                location:new google.maps.LatLng(stop["stop"].latitude, stop["stop"].longitude),
-                stopover:true});
-      });
-
-      var request = {
-          origin: start,
-          waypoints: waypts,
-          destination: end,
-          optimizeWaypoints: true,
-          travelMode: google.maps.TravelMode.WALKING
-      };
-
-      directionsService.route(request, function(response, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-          leg_durations = []
-          leg_lengths = []
-
-          // I need to loop through the response and create an array of durations
-          response.routes[0].legs.forEach(function(duration) {
-              leg_durations.push(duration.duration);
-          });
-
-          response.routes[0].legs.forEach(function(length) {
-              leg_lengths.push(length.distance);
-          });
-
-          $.ajax({
-            method: 'PUT',
-            url: x,
-            data: { tour_legs: leg_durations, leg_lengths: leg_lengths },
-            dataType: 'json'
-          });
-        }
-      });
-    };
-
-    function calc_route(stop_div_id, start_lat, start_long, end_lat, end_long) {
-     var start = new google.maps.LatLng(start_lat, start_long);
-     var end = new google.maps.LatLng(end_lat, end_long);
-
-     var request = {
-         origin: start,
-         destination: end,
-         travelMode: google.maps.TravelMode.WALKING
-     };
-
-     directionsService.route(request, function(response, status) {
-       if (status == google.maps.DirectionsStatus.OK) {
-         directionsDisplay.setDirections(response);
-       }
-     });
-
-     directionsDisplay.setPanel(document.getElementById(stop_div_id + '_directions_text'));
-
-    };
-  });
-};
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-$("#" + tour_data[0].div_id + '_directions').click(function() {
-  calc_route_to_start();
-});
-
-// This loop both declares which divs should trigger calcRoute, and passes in the parameters that calcRoute will use
-tour_data.forEach(function(stop) {
-  $("#" + stop.div_id + "_directions").click(function() {
-    calc_route(stop.div_id, stop.latitude, stop.longitude, tour_data[(stop.stop_number)].latitude, tour_data[(stop.stop_number)].longitude);
+      calc_route(stop.stop.div_id, stop["stop"].latitude, stop["stop"].longitude, json_tour_data["tour"]["tour_stops"][x]["stop"].latitude, json_tour_data["tour"]["tour_stops"][x]["stop"].longitude);
+    });
   });
 });
-
-calc_total_route();
